02939e05f02186286bb571b9d5563bd4c2405b4d
Trevor Merrifield <trevorm42@gmail.com>
2013-06-28 16:30:45 -0400

    started fleshing out get requests

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/messages", methods=['POST'])
+def messages():
+    start = request.args.get('start', default=None, type=int)
+    end = request.args.get('end', default=None, type=int)
+    return "start:" + start + " end:" + end
 
 if __name__ == "__main__":
     app.run()
