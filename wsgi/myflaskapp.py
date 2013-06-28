5b998b0c7f6d5aeaa77a86e03ab3a7c0e7f7c17b
Trevor Merrifield <trevorm42@gmail.com>
2013-06-28 17:28:25 -0400

    Stubbed more api requests, added venv to gitignore

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -6,11 +6,27 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/status", methods=['GET'])
+def status():
+    return "Not implemented"
+
 @app.route("/messages", methods=['GET'])
 def messages():
     start = request.args.get('start', default=None, type=int)
     end = request.args.get('end', default=None, type=int)
     return "start:" + str(start) + " end:" + str(end)
+
+@app.route("/users", methods=['GET'])
+def users():
+    return "Users go here"
+
+@app.route("/ops", methods=['GET'])
+def ops():
+    return "Operators go here"
+
+@app.route("/voiced", methods=['GET'])
+def voiced():
+    return "Voiced go here"
 
 if __name__ == "__main__":
     app.run()
