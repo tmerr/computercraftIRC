f1ec9b0dcc1dbdce64fd92baf6c737b919116076
Trevor Merrifield <trevorm42@gmail.com>
2013-07-01 03:19:20 -0400

    Make ChatPane a class and implement word wrap and much more. Much of the code is reworked and more flexible.

--- client.lua
+++ client.lua
@@ -3,8 +3,9 @@
 REMOTE = false     -- whether the script is being run remotely or locally
 DIVIDER_POS = 15  -- the divider between users and chat
 
-screen = peripheral.wrap("left")
-screen.clear()
+monitor = peripheral.wrap("left")
+monitor.clear()
+monitor.setCursorPos(1,1)
 
 --os.loadAPI("json")
 
@@ -58,61 +59,167 @@ function fetchVoiced()
 	return decodeJsonFrom(VOICED_URL)
 end
 
------------------------------------------------------------------------printing
+--------------------------------------------------------------------------stuff
 
-nickcolor = colors.gray
-messagecolor = colors.white
 dividercolor = colors.white
-screenx, screeny = term.getSize()
-maxhistory = 50
 
 function drawDivider()
-	for y=1,screeny do
+	for y=1,screenheight do
 		screen.setTextColor(dividercolor)
 		screen.setCursorPos(DIVIDER_POS, y)
 		screen.write("|")
 	end
 end
 
-chathistory = {}
-function addToChatPane(nick, message)	
-	table.insert(chathistory, {nick, message})
-	if (#chathistory > maxhistory) then
-		table.remove(chathistory, 1)
+----------------------------------------------------------------------chat pane
+ChatPane = {}
+ChatPane.__index = ChatPane
+
+function ChatPane.create(screen)
+	local self = {}
+	setmetatable(self, ChatPane)
+	
+	self.screen = screen
+	self.maxhistory = 50
+
+	local screenwidth, screenheight = self.screen.getSize()
+	self.left = DIVIDER_POS + 2
+	self.right = screenwidth
+	self.top = 1
+	self.bot = screenheight
+	self.height = self.bot - self.top + 1
+	self.width = self.right - self.left + 1
+
+	-- The history is everything written to the chat diced up into
+	-- characters and mashed together.
+	-- It's a sequence of {["char"], ["color"]}
+	self.history = {}
+	
+	return self
+end
+
+-- Get cursor position relative to this
+function ChatPane:getCursorPos()
+	local x, y = self.screen.getCursorPos()
+	x = x - self.left + 1
+	y = y - self.top + 1
+	return x, y
+end
+
+function ChatPane:setCursorPos(x, y)
+	local setx = self.left + x - 1
+	local sety = self.top + y - 1
+	self.screen.setCursorPos(setx, sety)
+end
+
+-- incomplete
+function ChatPane:position(left, right, top, bot)
+	self.left = left
+	self.right = right
+	self.top = top
+	self.bot = bot
+	-- update drawing
+end
+
+-- Write to the chat pane. Accepts \n characters. Color arg is optional.
+function ChatPane:write(text, color)
+	color = color or colors.white
+	for i=1,#text do
+		local ch = string.sub(text, i, i)
+		local pair = {["char"]=ch, ["color"]=color}
+		table.insert(self.history, pair)
+	end
+	self:draw()
+end
+
+function ChatPane:newLine()
+	local x, y = self:getCursorPos()
+	self:setCursorPos(1, y+1)
+end
+
+-- Draw the text word wrapped to the screen.
+function ChatPane:draw()
+	-- We only want to write the lines that fit on the screen.  So the
+	-- output is stored in a buffer and the bottom portion of it is sliced
+	-- off and displayed.
+	--
+	-- It is indexed by buffer[y[x["char" or "color"]]]
+	local buffer = {{}}
+	local buff_left = 1
+	local buff_right = self.width
+	local buff_top = 1
+
+	local buff_x = buff_left
+	local buff_y = buff_top
+	
+	for i=1,#self.history do
+		local el = self.history[i]
+		if el["char"] == "\n" then
+			-- clear the rest of the line and move to next line
+			local blank = {["char"]=" ", ["color"]=colors.white}
+			for x=buff_x,buff_right do
+				buffer[buff_y][x] = blank
+			end
+			buff_x, buff_y = buff_left, buff_y+1
+			buffer[buff_y] = {}
+		else
+			if buff_x <= buff_right then
+				-- write next character
+				buffer[buff_y][buff_x] = el
+				buff_x = buff_x + 1
+			else
+				-- no more space, write on the next line
+				buff_x, buff_y = buff_left, buff_y+1
+				buffer[buff_y] = {}
+				buffer[buff_y][buff_x] = el
+				buff_x = buff_x + 1
+			end
+		end
 	end
 
-	-- select the nick message pairs that fit on screen
-	top_element = #chathistory - screeny + 1
-	if (top_element < 1) then
-		top_element = 1
+	-- Make sure the last line has blank characters at the end
+	local blank = {["char"]=" ", ["color"]=colors.white}
+	for x=buff_x, buff_right do
+		buffer[buff_y][x] = blank
 	end
-	bot_element = #chathistory
 
-	-- write the nick message pairs
-	screenline = 1
-	for e=top_element, bot_element do
-		screen.setCursorPos(DIVIDER_POS + 2, screenline)
-		if screen.isColor() then
-			screen.setTextColor(nickcolor)
+	-- Actually write the text to the screen
+	self:setCursorPos(1,1)
+	local startread = #buffer - self.height + 1
+	if startread < 1 then
+		startread = 1
+	end
+	for linenum=startread,#buffer do
+		line = buffer[linenum]
+		for letternum=1,#line do
+			letter = line[letternum]
+			ch = letter["char"]
+			col = letter["color"]
+			if self.screen.isColor() then
+				self.screen.setTextColor(col)
+			end
+			self.screen.write(ch)
 		end
-		screen.write(chathistory[e][1]..": ")
-		if screen.isColor() then
-			screen.setTextColor(messagecolor)
+		if linenum ~= #buffer then
+			self:newLine()
 		end
-		screen.write(chathistory[e][2])
-		screenline = screenline + 1
 	end
 end
+
+---------------------------------------------------------------------other shit
 
 function printToUsersPane(text)
 end
 
 nextmsg = 0
-function updateMessages()
+function updateMessages(c)
 	messages = fetchMessages(nextmsg, nil)
 	while not (messages[tostring(nextmsg)] == nil) do
 		local entry = messages[tostring(nextmsg)]
-		addToChatPane(entry["nick"], entry["message"])
+		local nicktext, nickcol = entry["nick"]..": ", colors.gray
+		local msgtext, msgcol = entry["message"], colors.white
+		c:write("\n"..nicktext, nickcol)
+		c:write(msgtext, msgcol)
 		nextmsg = nextmsg + 1
 	end
 end
@@ -127,7 +234,8 @@ function printVoiced()
 end
 
 drawDivider()
+c = ChatPane.create(monitor)
 while true do
-	updateMessages()
+	updateMessages(c)
 	os.sleep(2)
 end
